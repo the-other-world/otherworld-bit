40548b632fba9577e0dd67c916d838e1b34820d9
daizihan233 <y066ydg5@duck.com>
2023-08-07 18:42:48 +0800

    说实话没发现任何bug，但我还是帮你格式化了一下代码（

--- modules/planetinfo.py
+++ modules/planetinfo.py
@@ -1,6 +1,7 @@
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.parser.base import MatchContent
 from graia.ariadne.model import Group
 from graia.saya import Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
@@ -14,9 +15,14 @@ channel.author("Abjust")
 
 @channel.use(
     ListenerSchema(
-        listening_events=[GroupMessage]
+        listening_events=[GroupMessage],
+        decorators=[MatchContent("!info")]
     )
 )
-async def display(app: Ariadne, group: Group, message: MessageChain):
-    if message.display == "!info":
-        await app.send_message(group, "机器人版本：OtherWorldBit 1.0.0\n上次更新日期：2023/8/7\n更新内容：OtherWorldBit 初始版本")
+async def display(app: Ariadne, group: Group):
+    await app.send_message(
+        group,
+        "机器人版本：OtherWorldBit 1.0.0\n"
+        "上次更新日期：2023/8/7\n"
+        "更新内容：OtherWorldBit 初始版本"
+    )
--- modules/version.py
+++ modules/version.py
@@ -1,10 +1,9 @@
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
-from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.parser.base import MatchContent
 from graia.ariadne.model import Group
 from graia.saya import Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
-
 
 channel = Channel.current()
 channel.name("显示版本")
@@ -14,9 +13,14 @@ channel.author("Abjust")
 
 @channel.use(
     ListenerSchema(
-        listening_events=[GroupMessage]
+        listening_events=[GroupMessage],
+        decorators=[MatchContent("版本")]
     )
 )
-async def display(app: Ariadne, group: Group, message: MessageChain):
-    if message.display == "版本":
-        await app.send_message(group, "机器人版本：OtherWorldBit 1.0.0\n上次更新日期：2023/8/7\n更新内容：OtherWorldBit 初始版本")
+async def display(app: Ariadne, group: Group):
+    await app.send_message(
+        group,
+        "机器人版本：OtherWorldBit 1.0.0\n"
+        "上次更新日期：2023/8/7\n"
+        "更新内容：OtherWorldBit 初始版本"
+    )
