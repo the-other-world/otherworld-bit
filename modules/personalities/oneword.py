601b01591bc406b07d14c52ab6468d5ddc234751
Abjust <2533898108@qq.com>
2023-10-10 09:14:37 -0700

    1.0.6

--- modules/personalities/oneword.py
+++ modules/personalities/oneword.py
@@ -1,3 +1,5 @@
+import random
+
 from graia.amnesia.message import MessageChain
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
@@ -7,13 +9,10 @@ from graia.saya import Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
 from graia.saya.channel import ChannelMeta
 
-import random
-
 channel = Channel[ChannelMeta].current()
 channel.meta['name'] = "mbti一言"
 channel.meta['description'] = "mbti一言"
 channel.meta['author'] = "ltzXiaoYanMo"
-
 
 words = open('./data/oneword.txt', encoding='utf-8').readlines()
 
@@ -26,15 +25,22 @@ words = open('./data/oneword.txt', encoding='utf-8').readlines()
 )
 async def mbti_oneword(app: Ariadne, group: Group, message: MessageChain = DetectPrefix("!oneword")):
     if str(message) != "":
+        has_word = False
         for word in words:
-            if word[-5:].strip() == str(message).strip().upper():
+            if word.strip()[-4:] == str(message).strip().upper():
+                has_word = True
                 await app.send_message(
                     group,
-                    word
+                    word.strip()
                 )
                 break
+        if not has_word:
+            await app.send_message(
+                group,
+                "找不到该MBTI类型！"
+            )
     else:
         await app.send_message(
             group,
-            words[random.randrange(0, len(words))]
+            words[random.randrange(0, len(words))].strip()
         )
